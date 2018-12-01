58c6103527c2bea7501717e5fba67f3748fe6753
John Thompson <jthompson4004@gmail.com>
2018-11-30 19:29:16 -0500

    Bye!

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -32,7 +32,33 @@ export class MapPage {
                 zoom: 11
             }
         });
-
+        this.map.addMarkerSync({
+            title: 'AFD Station 3',
+            snippet: 'Phone: (404) 555 - 3456',
+            position: {
+                lat: 33.75416,
+                lng: -84.37742
+            },
+            animation: GoogleMapsAnimation.BOUNCE
+        });
+        this.map.addMarkerSync({
+            title: 'BOYS & GIRLS CLUB W.W. WOOLFOLK',
+            snippet: 'Phone: (404) 555 - 1234',
+            position: {
+                lat: 33.73182,
+                lng: -84.43971
+            },
+            animation: GoogleMapsAnimation.BOUNCE
+        });
+        this.map.addMarkerSync({
+            title: 'PATHWAY UPPER ROOM CHRISTIAN MINISTRIES',
+            snippet: 'Phone: (404) 555 - 5432',
+            position: {
+                lat: 33.70866,
+                lng: -84.41853
+            },
+            animation: GoogleMapsAnimation.BOUNCE
+        });
     }
 
 }
